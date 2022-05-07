04c79d11928d0376682c90b0f1fe91b1f65dd501
S D Sriram <sdsriram1@gmail.com>
2022-05-07 21:45:20 +0530

    working

---
+++ chancge_val_of_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{   // instalize an array
+    int array[5] = {1,2,3,4,5};
+    //change  the value of the first array to 0
+    array[1] = 0 ;
+
+    // Change the 5th array to 6
+    array[5] = 6 ;
+    
+
+    return 0;
+}
